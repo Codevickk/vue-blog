9750b2cdeecbbc1c03475789d61d3c52164e83fc
codevickk <codevickk@gmail.com>
2021-12-14 09:24:43 +0100

    Fix fonts bug

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,42 @@ export default {
   },
 };
 </script>
+
+<style>
+/* Include SF Pro Display */
+@font-face {
+  font-family: "SF Pro Display";
+  src: url("./assets/fonts/SF-Pro-Display-Black.otf");
+  font-weight: 900;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "SF Pro Display";
+  src: url("./assets/fonts/SF-Pro-Display-Medium.otf");
+  font-weight: 500;
+  font-style: normal;
+}
+
+/* Include SF Pro Text */
+@font-face {
+  font-family: "SF Pro Text";
+  src: url("./assets/fonts/SF-Pro-Text-Bold.otf");
+  font-weight: 500;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "SF Pro Medium";
+  src: url("./assets/fonts/SF-Pro-Text-Medium.otf");
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "SF Pro Text";
+  src: url("./assets/fonts/SF-Pro-Text-Regular.otf");
+  font-weight: 300;
+  font-style: normal;
+}
+</style>
