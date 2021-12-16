d5067521444f99eab892b8642405163acef5fefa
codevickk <codevickk@gmail.com>
2021-12-16 11:20:07 +0100

    Make application a PWA

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 
 import './assets/styles/normalize.css';
 import './assets/styles/scss/main.scss';
+import './registerServiceWorker'
 
 createApp(App).use(router).mount('#app')
