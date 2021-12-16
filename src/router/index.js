daf793be1f2def3f665c70e396078176903ddde5
codevickk <codevickk@gmail.com>
2021-12-16 11:13:30 +0100

    Code splitting for the routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import SinglePost from '../views/SinglePost.vue';
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: () => import("../views/Home.vue"),
   },
   {
-    path: '/posts/:id',
-    name: 'SinglePost',
-    component: SinglePost
-  }
-]
+    path: "/posts/:id",
+    name: "SinglePost",
+    component: () => import("../views/SinglePost.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
